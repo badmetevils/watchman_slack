765983830ae73eef8a907c60381d9077406f01ce
Tanuj Sharma <tanujsharma1711@gmail.com>
2020-07-31 09:49:00 +0530

    debug issue

--- src/rtm.ts
+++ src/rtm.ts
@@ -1,9 +1,28 @@
+import logger from '@shared/Logger';
 import { RTMClient, LogLevel } from '@slack/rtm-api';
 
 const token = process.env.LEGACY_SLACK_BOT_TOKEN || '';
 
 export const watchmanRTM = new RTMClient(token, {
-  useRtmConnect: true,
-  //   logLevel: LogLevel.DEBUG,
-  autoReconnect: true
+  logLevel: LogLevel.DEBUG,
+  useRtmConnect: true
+  // logger: {
+  //   debug(...msg) {
+  //     logger.log('debug', msg);
+  //   },
+  //   error(...msg) {
+  //     logger.log('error', msg);
+  //   },
+  //   info(...msg) {
+  //     logger.log('info', msg);
+  //   },
+  //   warn(...msg) {
+  //     logger.log('warn', msg);
+  //   },
+  //   setLevel() {},
+  //   setName() {},
+  //   getLevel() {
+  //     return LogLevel.DEBUG;
+  //   }
+  // }
 });
