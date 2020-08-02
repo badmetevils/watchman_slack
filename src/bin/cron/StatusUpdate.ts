1159f32454f43d2c4dc8eecceb568cfafd07d62e
Tanuj Sharma <tanujsharma1711@gmail.com>
2020-08-02 23:10:47 +0530

    Added resubscription over cron instead of bolt

--- src/bin/cron/StatusUpdate.ts
+++ src/bin/cron/StatusUpdate.ts
@@ -1,9 +1,6 @@
-import { addUserStatusLog } from '@models/queries';
 import cron from 'node-cron';
-import CheckAwayStatus from '@watchman-hooks/CheckAwayStatus';
 import logger from '@shared/Logger';
-import { IPresenceData } from '@typing/presence';
-import time from '@lib/time';
+import Presence from '@bin/Presence';
 
 export default class StatusUpdateAt12 {
   public run() {
@@ -13,28 +10,11 @@ export default class StatusUpdateAt12 {
       ' 0 05 0 * * * *',
       async () => {
         try {
-          const userPresence: IPresenceData[] | undefined = await CheckAwayStatus.allMembers();
-          if (!!userPresence) {
-            for (const { slackID, status } of userPresence) {
-              const timestamp = time().toMySqlDateTime().toString();
-              const log = {
-                slackID,
-                status,
-                timestamp
-              };
-              // console.log({ log });
-              const record = await addUserStatusLog(log);
-              if (!!record) {
-                logger.info(
-                  `A new entry created for ${record.getDataValue('slackID')} at table "${
-                    record.constructor.name
-                  }" Via Cron`
-                );
-              }
-            }
-          }
+          const presence = new Presence();
+          await presence.subscribe();
+          await presence.listen();
         } catch (error) {
-          logger.error('An error occurred when fetching status on cron');
+          logger.error('An error occurred while resubscribing on cron');
         }
       },
       {
